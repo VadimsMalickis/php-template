be00027d77817b4f35b15790b716f4ab1b918a00
VadimsMalickis <vadims.malickis@rvt.lv>
2023-02-07 18:25:40 +0200

    project template update

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 // Composer class autoloader
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 //Remove it later
 echo '<h1 style="text-align: center">Welcome to PHP</h1>';
---
+++ src/DefaultController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class DefaultController
+{
+    public function indexAction()
+    {
+
+    }
+}
